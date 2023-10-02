05c510483366dd1b6b641453c816a155811a08c5
Nrdias <natandias.dev@gmail.com>
2023-10-02 19:42:54 -0300

    base do projeto

---
+++ src/ListaOrdenadaDePalavras.java
@@ -0,0 +1,27 @@
+/**
+ * Esta classe guarda as palavra do indice remissivo em ordem alfabetica.
+ * @author Isabel H. Manssour
+ */
+
+public class ListaOrdenadaDePalavras {
+
+    // Classe interna 
+    private class Palavra {
+        public String s;
+        public ListaDeOcorrencias listaOcorrencias;
+        public Palavra next;    
+        public Palavra(String str) {
+            s = str;
+            next = null;
+            listaOcorrencias = new ListaDeOcorrencias();
+        }
+        
+        // Metodos
+
+    }
+    
+    // Atributos
+    
+    // Metodos
+
+}
